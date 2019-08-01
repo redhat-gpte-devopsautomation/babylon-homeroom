8fa30a750c66f17972f55a26f474d1210130c045
Pavel Anni <panni@redhat.com>
2019-08-01 17:38:36 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify
+import os
+import socket
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+
+# Check if we are running inside a Pod by checking this file
+if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/namespace"):
+# current namespace thanks to Ralph: https://github.com/kubernetes-client/python/issues/363
+    cr_namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
+    config.load_incluster_config()
+else:
+# current namespace thanks to Ralph: https://github.com/kubernetes-client/python/issues/363
+    config.load_kube_config()
+    cr_namespace = config.list_kube_config_contexts()[1]['context']['namespace']
+
+cr_group = 'gpte.redhat.com'
+cr_version = 'v1'
+cr_plural = 'resourceclaims'
+api = client.CustomObjectsApi()
+response = api.list_namespaced_custom_object(cr_group, cr_version, cr_namespace, cr_plural)
+cr_name = response['items'][0]['metadata']['name']
+
+body_start = {"spec":{"desiredState":"running"}}
+body_stop = {"spec":{"desiredState":"stopped"}}
+
+app = Flask(__name__)
+
+@app.route("/version")
+def version():
+    return os.getenv("VERSION", "0.0")
+
+@app.route("/start")
+def start():
+    try:
+        response = api.patch_namespaced_custom_object(
+            group=cr_group,
+            version=cr_version,
+            plural=cr_plural,
+            name=cr_name, 
+            namespace=cr_namespace, 
+            body=body_start)
+    except ApiException as e:
+        print("Exception when calling \
+              CustomObjectsApi->patch_namespaced_custom_object: %s\n" % e)
+    return "STARTED"
+
+@app.route("/stop")
+def stop():
+    try:
+        response = api.patch_namespaced_custom_object(
+            group=cr_group,
+            version=cr_version,
+            plural=cr_plural,
+            name=cr_name, 
+            namespace=cr_namespace, 
+            body=body_stop)
+    except ApiException as e:
+        print("Exception when calling \
+              CustomObjectsApi->patch_namespaced_custom_object: %s\n" % e)
+    return "STOPPED"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
