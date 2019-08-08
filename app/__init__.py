798ba5c86a7fce457a9c71eb83136a1c5c3769ee
pavelanni <panni@redhat.com>
2019-08-07 21:25:40 -0400

    Fixed patch strings; buttons are now disabled when appropriate

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
+"""
+This module runs a simple Flask-based front-end to Babylon/Anarchy 
+ResourceClaims
+"""
+import os
+import socket
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 from app.forms import ResourceForm
-import os
-import socket
-from kubernetes import client, config
-from kubernetes.client.rest import ApiException
-
 
 # Check if we are running inside a Pod by checking this file
 if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/namespace"):
@@ -26,8 +29,8 @@ api = client.CustomObjectsApi()
 response = api.list_namespaced_custom_object(cr_group, cr_version, cr_namespace, cr_plural)
 cr_name = response['items'][0]['metadata']['name']
 
-body_start = {"spec":{"desiredState":"running"}}
-body_stop = {"spec":{"desiredState":"stopped"}}
+body_start = {"spec":{"template":{"spec":{"desiredState": "deployed"}}}}
+body_stop = {"spec":{"template":{"spec":{"desiredState": "idled"}}}}
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -42,17 +45,31 @@ def index():
                                                   cr_namespace, cr_plural,
                                                   cr_name)
     form = ResourceForm()
+    if (cr_status['status']['resource']['status']['state'] == 'deployed' or
+        cr_status['status']['resource']['status']['state'] == 'idling' or
+        cr_status['status']['resource']['status']['state'] == 'idle-pending' or 
+        cr_status['status']['resource']['status']['state'] == 'resuming' or 
+        cr_status['status']['resource']['status']['state'] == 'resume-pending') :
+        form.btn_start.render_kw = {'disabled':''}
+    if (cr_status['status']['resource']['status']['state'] == 'idled' or
+        cr_status['status']['resource']['status']['state'] == 'idling' or
+        cr_status['status']['resource']['status']['state'] == 'resuming' or 
+        cr_status['status']['resource']['status']['state'] == 'idle-pending') :
+        form.btn_stop.render_kw = {'disabled':''}
     if request.method == 'POST':
         result = request.form
         if 'btn_stop' in result.keys():
+            form.btn_stop.render_kw = {'disabled':''}
             stop()
         if 'btn_start' in result.keys():
+            form.btn_start.render_kw = {'disabled':''}
             start()
         if 'btn_update' in result.keys():
             cr_status = api.get_namespaced_custom_object(cr_group, cr_version,
                                                   cr_namespace, cr_plural,
                                                   cr_name)
-            
+        return redirect('/index')
+
     return render_template('index.html', form=form, 
                                cr_status=cr_status)
 
