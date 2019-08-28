ec876ca13d5a1170b265b775f7e12f67d66aef49
Pavel Anni <panni@redhat.com>
2019-08-28 18:50:41 -0400

    Recreate buttons; display status when starting; proper delete RC

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ This module runs a simple Flask-based front-end to Babylon/Anarchy
 ResourceClaims
 """
 import os
-import socket
+import time
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
 from flask import Flask, render_template, flash, redirect, url_for, request
@@ -23,11 +23,17 @@ cr_group = 'poolboy.gpte.redhat.com'
 cr_version = 'v1'
 cr_plural = 'resourceclaims'
 api = client.CustomObjectsApi()
-response = api.list_namespaced_custom_object(cr_group, cr_version, cr_namespace, cr_plural)
-cr_name = response['items'][0]['metadata']['name']
+cr_name = ''
 
 body_start = {"spec":{"template":{"spec":{"desiredState": "started"}}}}
 body_stop = {"spec":{"template":{"spec":{"desiredState": "stopped"}}}}
+
+btn_classes = {
+    'start':  ['btn', 'btn-success'],
+    'stop':   ['btn', 'btn-primary'],
+    'delete': ['btn', 'btn-danger'],
+    'info':   ['btn', 'btn-info'],
+}
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -38,11 +44,52 @@ app.config['SECRET_KEY'] = SECRET_KEY
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    global cr_name
+    try:
+        response = api.list_namespaced_custom_object(cr_group,
+                                                     cr_version,
+                                                     cr_namespace,
+                                                     cr_plural)
+    except ApiException:
+        return render_template('notfound.html')
+
+    try:
+        cr_name = response['items'][0]['metadata']['name']
+    except IndexError:
+        return render_template('notfound.html')
+
+
     cr_status = api.get_namespaced_custom_object(cr_group, cr_version,
                                                  cr_namespace, cr_plural,
                                                  cr_name)
 
+    if (cr_status['spec']['template']['spec']['desiredState'] == 'stopped' and
+            len(btn_classes['stop']) < 3):
+        btn_classes['stop'].append('disabled')
+
+    if (cr_status['spec']['template']['spec']['desiredState'] == 'started' and
+            len(btn_classes['start']) < 3):
+        btn_classes['start'].append('disabled')
+
+    try:
+        curr_state = cr_status['status']['resource']['status']['state']
+    except KeyError:
+        return render_template('starting.html')
+
+
+    if (cr_status['status']['resource']['status']['state'] == 'stopped' and 
+        cr_status['spec']['template']['spec']['desiredState'] == 'stopped' and
+        len(btn_classes['start']) >= 3):
+        btn_classes['start'].pop(-1)
+
+    if (cr_status['status']['resource']['status']['state'] == 'started' and
+        cr_status['spec']['template']['spec']['desiredState'] == 'started' and
+        len(btn_classes['stop']) >= 3):
+        btn_classes['stop'].pop(-1)
+
+    
     return render_template('index.html', 
+                           btn_classes=btn_classes,
                            cr_status=cr_status)
 
 @app.route('/version')
@@ -54,6 +101,11 @@ def start():
     cr_status = api.get_namespaced_custom_object(cr_group, cr_version,
                                                  cr_namespace, cr_plural,
                                                  cr_name)
+# Disable Start and enable Stop buttons
+    if len(btn_classes['start']) < 3:
+        btn_classes['start'].append('disabled')
+    if len(btn_classes['stop']) >= 3:
+        btn_classes['stop'].pop(-1)
     try:
         response = api.patch_namespaced_custom_object(
             group=cr_group,
@@ -65,14 +117,18 @@ def start():
     except ApiException as e:
         print("Exception when calling \
               CustomObjectsApi->patch_namespaced_custom_object: %s\n" % e)
-    return render_template('index.html', 
-                           cr_status=cr_status)
+    return redirect(url_for('index'))
 
 @app.route('/stop')
 def stop():
     cr_status = api.get_namespaced_custom_object(cr_group, cr_version,
                                                  cr_namespace, cr_plural,
                                                  cr_name)
+# Disable Stop and enable Start buttons
+    if len(btn_classes['stop']) < 3:
+        btn_classes['stop'].append('disabled')
+    if len(btn_classes['start']) >= 3:
+        btn_classes['start'].pop(-1)
     try:
         response = api.patch_namespaced_custom_object(
             group=cr_group,
@@ -84,8 +140,7 @@ def stop():
     except ApiException as e:
         print("Exception when calling \
               CustomObjectsApi->patch_namespaced_custom_object: %s\n" % e)
-    return render_template('index.html', 
-                           cr_status=cr_status)
+    return redirect(url_for('index'))
 
 @app.route('/delete')
 def delete():
@@ -104,6 +159,5 @@ def delete():
     except ApiException as e:
         print("Exception when calling \
               CustomObjectsApi->patch_namespaced_custom_object: %s\n" % e)
-    return render_template('index.html', 
-                           cr_status=cr_status)
+    return redirect(url_for('index'))
 
